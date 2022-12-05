ad78e15258e0b2fcb3324fc281f2896f1b5b672e
Joe DeSoto <modulusman@gmail.com>
2022-12-05 15:02:24 -0800

    Updates index.js to use app.use() method instead to serve files from the project's public folder when the server is pinged.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.port || 4000;
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.send("Hello, world from express! Now using nodemon :D");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Example express app listening on port ${port}`);
