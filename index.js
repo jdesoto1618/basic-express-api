c40e8daf7d30714c4be9161ecb8043f2d49eb226
Joe DeSoto <modulusman@gmail.com>
2022-12-05 15:12:49 -0800

    Adds hardcoded members array with three entries. Sets an API endpoint to be /api/members.

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const app = express();
 const port = process.env.port || 4000;
 const path = require("path");
 
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@email.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bob Sagat",
+    email: "bobs@email.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Shannon O'Shannon",
+    email: "shannonx2@email.com",
+    status: "active",
+  },
+];
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
