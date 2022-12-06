a610a4fe3064294dccd006328b2f1d017d753835
Joe DeSoto <modulusman@gmail.com>
2022-12-06 10:04:06 -0800

    Adds some() method to help check whether a user exists from a user's id.
    If a user does not exist, the API will throw a 400 error with
    a message.

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ app.get("/api/members", (req, res) => {
   res.json(members);
 });
 
+app.get("/api/members/:id", (req, res) => {
+  const memberExists = members.some(
+    (member) => member.id === parseInt(req.params.id, 10)
+  );
+
+  if (!memberExists) {
+    res.status(400).json({ error: `Member ${req.params.id} not found` });
+    return false;
+  }
+
+  res.json(
+    members.filter((member) => member.id === parseInt(req.params.id, 10))
+  );
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
