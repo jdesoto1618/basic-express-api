4aea85670defa49068529b718dde2d956697e1a7
Joe DeSoto <modulusman@gmail.com>
2022-12-06 09:30:30 -0800

    Moves middleware into its own folder. For now, removes the logger middelware call in index js as it was only there for demo purposees.

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 4000;
 const path = require("path");
-const moment = require("moment");
 const members = require("./Members");
-
-const logger = (req, res, next) => {
-  console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
-  next();
-};
-
-app.use(logger);
 
 app.get("/api/members", (req, res) => {
   res.json(members);
