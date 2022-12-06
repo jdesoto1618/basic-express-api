8376523e7991883d84629b5af5fcde88f59472dc
Joe DeSoto <modulusman@gmail.com>
2022-12-05 16:00:28 -0800

    Moves members into its own file and requies that file in index.js.

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@email.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bob Sagat",
+    email: "bobs@email.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Shannon O'Shannon",
+    email: "shannonx2@email.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -2,27 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 4000;
 const path = require("path");
-
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@email.com",
-    status: "active",
-  },
-  {
-    id: 2,
-    name: "Bob Sagat",
-    email: "bobs@email.com",
-    status: "inactive",
-  },
-  {
-    id: 3,
-    name: "Shannon O'Shannon",
-    email: "shannonx2@email.com",
-    status: "active",
-  },
-];
+const members = require("./Members");
 
 app.get("/api/members", (req, res) => {
   res.json(members);
