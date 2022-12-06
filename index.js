1bcfb3fbbf29c96b44047d37dd5eee0f8eeb58fc
Joe DeSoto <modulusman@gmail.com>
2022-12-06 10:15:36 -0800

    Moves the common routes for the API into a routes folder.

--- index.js
+++ index.js
@@ -2,28 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 4000;
 const path = require("path");
-const members = require("./Members");
-
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-app.get("/api/members/:id", (req, res) => {
-  const memberExists = members.some(
-    (member) => member.id === parseInt(req.params.id, 10)
-  );
-
-  if (!memberExists) {
-    res.status(400).json({ error: `Member ${req.params.id} not found` });
-    return false;
-  }
-
-  res.json(
-    members.filter((member) => member.id === parseInt(req.params.id, 10))
-  );
-});
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", require("./routes/api/members"));
 
 app.listen(port, () => {
   console.log(`Example express app listening on port ${port}`);
---
+++ routes/api/members.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const members = require("../../Members");
+
+router.get("/", (req, res) => {
+  res.json(members);
+});
+
+router.get("/:id", (req, res) => {
+  const memberExists = members.some(
+    (member) => member.id === parseInt(req.params.id, 10)
+  );
+
+  if (!memberExists) {
+    res.status(400).json({ error: `Member ${req.params.id} not found` });
+    return false;
+  }
+
+  res.json(
+    members.filter((member) => member.id === parseInt(req.params.id, 10))
+  );
+});
+
+module.exports = router;
